66c9ce413fcdafc603555850fb627ffc5acd2ab4
KhoaVD <khoavdse171494@fpt.edu.vn>
2024-09-22 22:11:34 +0700

    Create Repo, Service and some stuff

---
+++ KoiShop/BusinessObject/Service/UserService.cs
@@ -0,0 +1,13 @@
+﻿using BusinessObject.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Service
+{
+    public class UserService:IUserService
+    {
+    }
+}
---
+++ KoiShop/DataAccess/Repo/UserRepo.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Entity;
+using DataAccess.IRepo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repo
+{
+    public class UserRepo(DbContext context) : BaseRepo<User>(context), IUserRepo
+    {
+        private readonly DbContext _context = context;
+
+        public Task<User> CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
