41bbbddcb6eda37065946a502a4820ac1eb23353
VanMinhtp <vanminh101202@gmail.com>
2024-10-20 04:54:01 +0700

    Update user service

--- KoiShop/KoiShopController/Controllers/UserController.cs
+++ KoiShop/KoiShopController/Controllers/UserController.cs
@@ -23,53 +23,10 @@ namespace KoiShopController.Controllers
             _configuration = configuration;
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> CreateUser(CreateUserDTO newUser)
-        {
-            var result = await _userService.CreateUser(newUser);
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result.Message);
-            }
-        }
-
-        [HttpPost("CreateStaff")]
-        [Authorize(Roles = "Admin, Manager")]
-        public async Task<IActionResult> CreateStaff(CreateUserDTO newStaff)
-        {
-            var result = await _userService.CreateStaff(newStaff);
-
-            if (!result.Success)
-            {
-                return BadRequest(result.Message);
-            }
-
-            return Ok(result);
-        }
-
-        [HttpPost("CreateManager")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> CreateManager(CreateUserDTO newManager)
-        {
-            var result = await _userService.CreateManager(newManager);
-
-            if (!result.Success)
-            {
-                return BadRequest(result.Message);
-            }
-
-            return Ok(result);
-        }
-
         [HttpGet]
         [Authorize(Roles = "Admin, Manager, Staff")]
-        public async Task<IActionResult> GetUser(int page=1, int pageSize=10,
-            string search="", string sort="")
+        public async Task<IActionResult> GetUser(int page = 1, int pageSize = 10,
+            string search = "", string sort = "")
         {
             var result = await _userService.GetAllUser(page, pageSize, search, sort);
             if (result.Success)
@@ -91,7 +48,10 @@ namespace KoiShopController.Controllers
             {
                 return Unauthorized("User ID not found in token.");
             }
-            var currentUserId = int.Parse(userIdClaim);
+            if (!int.TryParse(userIdClaim, out var currentUserId))
+            {
+                return Unauthorized("Invalid user ID format in token.");
+            }
 
             var currentUserRole = User.FindFirst(ClaimTypes.Role)?.Value;
             if (string.IsNullOrEmpty(currentUserRole))
@@ -113,7 +73,50 @@ namespace KoiShopController.Controllers
             return Ok(result);
         }
 
-        [HttpPost("Login")]
+        [HttpPost("createAccount")]
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateUser(CreateUserDTO newUser)
+        {
+            var result = await _userService.CreateUser(newUser);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.Message);
+            }
+        }
+
+        [HttpPost("createStaff")]
+        [Authorize(Roles = "Admin, Manager")]
+        public async Task<IActionResult> CreateStaff(CreateUserDTO newStaff)
+        {
+            var result = await _userService.CreateStaff(newStaff);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("createManager")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateManager(CreateUserDTO newManager)
+        {
+            var result = await _userService.CreateManager(newManager);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
         [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequest)
         {
@@ -125,57 +128,17 @@ namespace KoiShopController.Controllers
             var user = result.Data;
             var claims = new List<Claim>
             {
-               new Claim(ClaimTypes.Name, user.Name),
-               new Claim(ClaimTypes.NameIdentifier, user.Email),
-               new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-               new Claim(ClaimTypes.Role, user.Role.RoleName)
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, user.Role.RoleName)
             };
 
             // Tạo token
             var token = CreateToken(claims);
-            return Ok(new { Token = token, Role = user.Role.RoleName });
+            return Ok(new { Token = token, UserId = user.UserId, Role = user.Role.RoleName });
         }
-
-
-        //[HttpGet("Login/Manager/{email}, {pass}")]
-        //public async Task<IActionResult> LoginAdmin(string email, string pass)
-        //{
-        //    var result = await _userService.LoginAdmin(email, pass);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    else
-        //    {
-        //        return NotFound(result.Message);
-        //    }
-        //}
-        //[HttpGet("Login/Customer/{email}, {pass}")]
-        //public async Task<IActionResult> LoginCustomer(string email, string pass)
-        //{
-        //    var result = await _userService.LoginCustomer(email, pass);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    else
-        //    {
-        //        return NotFound(result.Message);
-        //    }
-        //}
-        //[HttpGet("Login/Staff/{email}, {pass}")]
-        //public async Task<IActionResult> LoginStaff(string email, string pass)
-        //{
-        //    var result = await _userService.LoginStaff(email, pass);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    else
-        //    {
-        //        return NotFound(result.Message);
-        //    }
-        //}
 
         [HttpPut("updateUser/{userId}")]
         [Authorize(Roles = "Admin, Manager, Staff")]
@@ -191,19 +154,77 @@ namespace KoiShopController.Controllers
                 return NotFound(result.Message);
             }
         }
-        [HttpDelete("{userId}")]
-        [Authorize(Roles = "Admin, Manager")]
-        public async Task<IActionResult> RemoveUser(int userId)
+
+        [HttpPost("request-password-reset")]
+        [AllowAnonymous]
+        public async Task<IActionResult> RequestPasswordReset([FromBody] RequestPasswordResetDTO request)
         {
-            var result=await _userService.RemoveUser(userId);
-            if (result.Success)
+            var result = await _userService.GeneratePasswordResetToken(request.Email);
+            if (!result)
             {
-                return Ok(result);
+                return BadRequest("Invalid email address.");
             }
-            else
+
+            return Ok("Password reset email has been sent.");
+        }
+
+        [HttpPost("reset-password")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO resetPasswordDto)
+        {
+            var result = await _userService.ResetPassword(resetPasswordDto.Token, resetPasswordDto.NewPassword);
+            if (!result)
             {
-                return NotFound(result.Message);
+                return BadRequest("Invalid or expired token.");
             }
+
+            return Ok("Password has been reset successfully.");
+        }
+
+        [HttpPut("updateProfile/{userId}")]
+        [Authorize(Roles = "Admin, Manager, Staff, Customer")]
+        public async Task<IActionResult> UpdateProfile(int userId, [FromBody] UpdateProfileDTO updateProfileDTO)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+            {
+                return Unauthorized("User ID not found in token.");
+            }
+            if (!int.TryParse(userIdClaim, out var currentUserId))
+            {
+                return Unauthorized("Invalid user ID format in token.");
+            }
+
+            var currentUserRole = User.FindFirst(ClaimTypes.Role)?.Value;
+            if (string.IsNullOrEmpty(currentUserRole))
+            {
+                return Unauthorized("User role not found in token.");
+            }
+
+            if (currentUserRole == "Customer" && currentUserId != userId)
+            {
+                return StatusCode(403, new { message = "You are not authorized to view other users' information." });
+            }
+
+            if (!string.IsNullOrEmpty(updateProfileDTO.NewPassword))
+            {
+                if (updateProfileDTO.NewPassword != updateProfileDTO.ConfirmPassword)
+                {
+                    return BadRequest("New Password and Confirm Password do not match.");
+                }
+
+                var isOldPasswordValid = await _userService.ValidateOldPassword(userId, updateProfileDTO.OldPassword);
+                if (!isOldPasswordValid)
+                {
+                    return BadRequest("Old Password is incorrect.");
+                }
+            }
+            var result = await _userService.UpdateProfile(userId, updateProfileDTO);
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
         }
 
         [HttpPut("deleteUser/{userId}")]
@@ -230,18 +251,20 @@ namespace KoiShopController.Controllers
             return Ok(result);
         }
 
-        [HttpPut("updateProfile/{userId}")]
-        [Authorize(Roles = "Admin, Manager, Staff, Customer")]
-        public async Task<IActionResult> UpdateProfile(int userId, [FromBody] UpdateProfileDTO updateProfileDTO)
+        [HttpDelete("deleteHard/{userId}")]
+        [Authorize(Roles = "Admin, Manager")]
+        public async Task<IActionResult> RemoveUser(int userId)
         {
-            var result = await _userService.UpdateProfile(userId, updateProfileDTO);
-            if (!result.Success)
+            var result = await _userService.RemoveUser(userId);
+            if (result.Success)
             {
-                return BadRequest(result.Message);
+                return Ok(result);
             }
-            return Ok(result);
+            else
+            {
+                return NotFound(result.Message);
+            }
         }
-
         private string CreateToken(List<Claim> claims)
         {
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
@@ -251,36 +274,11 @@ namespace KoiShopController.Controllers
                                    issuer: _configuration["Jwt:Issuer"],
                                    audience: _configuration["Jwt:Audience"],
                                    claims: claims,
-                                   expires: DateTime.UtcNow.AddHours(1),
+                                   expires: DateTime.UtcNow.AddHours(3),
                                    signingCredentials: cred
              );
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
             return jwt;
-        }
-        [HttpPost("request-password-reset")]
-        [AllowAnonymous]
-        public async Task<IActionResult> RequestPasswordReset([FromBody] RequestPasswordResetDTO request)
-        {
-            var result = await _userService.GeneratePasswordResetToken(request.Email);
-            if (!result)
-            {
-                return BadRequest("Invalid email address.");
-            }
-
-            return Ok("Password reset email has been sent.");
-        }
-
-        [HttpPost("reset-password")]
-        [AllowAnonymous]
-        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO resetPasswordDto)
-        {
-            var result = await _userService.ResetPassword(resetPasswordDto.Token, resetPasswordDto.NewPassword);
-            if (!result)
-            {
-                return BadRequest("Invalid or expired token.");
-            }
-
-            return Ok("Password has been reset successfully.");
         }
     }
 }
